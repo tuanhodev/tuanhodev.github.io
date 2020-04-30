28931939449b7f4ea1ec8638d17eedd0bdfaa792
TuanAnhDev <tuanho.design@outlook.com>
2020-04-30 19:00:51 +0700

    a commit

---
+++ assets/js/thd.js
@@ -0,0 +1,53 @@
+
+function social() {
+  return {
+    brands: [
+      { key: 'Youtube' },
+      { key: 'Facebook' },
+      { key: 'Zalo' },
+    ]
+  }
+}
+
+function cardsData() {
+  return {
+    cards: [
+      { key: 'Php', class: 'is-php', },
+      { key: 'Javascript', class: 'is-javascript', },
+      { key: 'Vim', class: 'is-vim', },
+      { key: 'Laravel', class: 'is-laravel', },
+      { key: 'Vuejs', class: 'is-vuejs', },
+      { key: 'Alpinejs', class: 'is-alpinejs', },
+      { key: 'Bootstrap', class: 'is-bootstrap', },
+      { key: 'Tailwindcss', class: 'is-tailwindcss', },
+    ]
+  }
+}
+
+function cardsData50100() {
+  return {
+    cards: [
+      { key: 'Php', class: 'is-php', },
+      { key: 'Javascript', class: 'is-javascript', },
+      { key: 'Vim', class: 'is-vim', },
+      { key: 'Laravel', class: 'is-laravel', },
+    ]
+  }
+}
+
+function headerModuleData() {
+  return {
+    headerModuleOne: {
+      title: 'H3 Title header in module 1', 
+      description: 'Is description in header module 1',
+    },
+    headerModuleTwo: {
+      title: 'H3 Title header in module 2', 
+      description: 'Is description in header module 2',
+    },
+  }
+}
+
+
+
+
