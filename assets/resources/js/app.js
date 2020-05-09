004f3cf88c972233d02710588865643a07d1fbab
TuanAnhDev <tuanho.design@outlook.com>
2020-05-09 14:43:04 +0700

    commit

--- assets/resources/js/app.js
+++ assets/resources/js/app.js
@@ -1,12 +1,12 @@
-/**
+
 import Vue from 'vue';
 window.Vue = require('vue');
-Vue.component('notifyComponent', require('./components/notify.vue').default);
+
+//Vue.component('notifyComponent', require('./components/notify.vue').default);
 
 const app = new Vue({
     el: '#app',
 });
- */
 
 'use strict'
 let Alpine = require('alpinejs');
